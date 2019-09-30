a07c9af8fa1e3c012b727da21e0463bfd99a7ee5
pdichone <pdichone@gmail.com>
2019-09-30 13:36:40 -0700

    Initial Commit

---
+++ BizCard-XC11/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BizCard-XC11
+//
+//  Created by Paulo Dichone on 9/30/19.
+//  Copyright © 2019 Paulo Dichone. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
