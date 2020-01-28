072799441dd33b21f6babe485ded3fafb4d99f80
mukesh_lokare <mukesh.lokare@wwindia.com>
2020-01-28 11:24:32 +0530

    Initial commit

--- BizCard-XC11/ContentView.swift
+++ BizCard-XC11/ContentView.swift
@@ -1,16 +1,48 @@
-//
-//  ContentView.swift
-//  BizCard-XC11
-//
-//  Created by Paulo Dichone on 9/30/19.
-//  Copyright © 2019 Paulo Dichone. All rights reserved.
-//
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        
+        HStack {
+            
+            Image("paulos-nice-photo")
+                .resizable()
+            .scaledToFit()
+                .clipShape(Circle())
+                .overlay(Circle()
+                    .stroke(Color.white, lineWidth: 3))
+                .shadow(radius: 5)
+//                .frame(width: 180, height:180, alignment: .center)
+            
+            
+            VStack() {
+                Text("Paulo Dichone")
+                    .font(.title)
+                    .foregroundColor(.white)
+                
+                Text("buildappswithpaulo.com")
+                    .font(.subheadline)
+                    .foregroundColor(.white)
+                HStack{
+                    Image(systemName: "t.square.fill")
+                        .foregroundColor(.pink)
+                    
+                    Text(": @buildappswithme")
+                        .font(.subheadline)
+                        .foregroundColor(Color.white)
+                        .bold()
+                        .italic()
+                    
+                }
+            }
+            
+        }.frame(width: 380, height: 200)
+        .background(Color.orange)
+        .cornerRadius(8)
+        .shadow(radius: 5)
+        
+        
     }
 }
 
